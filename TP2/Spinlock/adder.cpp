c49f5f8dd1a0f468e4db55b3e414eaed77c0bd78
JULIA-YOGA\jutogashi <jutogashi@gmail.com>
2023-05-02 23:36:40 +0200

    Adding TP2 Spinlock Adder

---
+++ TP2/Spinlock/adder.cpp
@@ -0,0 +1,139 @@
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <fstream>
+#include <bits/stdc++.h>
+#include <thread>
+#include <vector>
+#include <atomic>
+
+using namespace std;
+
+/**
+ * @brief Class implementing the spinlock using atomic function test and set.
+ * 
+ * Method acquire checks if lock is available and take it; or busy waits.
+ * Method release, releases lock for other thread to take it and enter cr.
+ */
+class SpinLock {
+public:
+  SpinLock() : flag_(ATOMIC_FLAG_INIT) {}
+
+  void acquire() {
+    while (flag_.test_and_set(memory_order_acquire)) {
+      cout<<"Waiting for lock release"<<endl;
+      // busy-wait
+    }
+  }
+
+  void release() {
+    flag_.clear(memory_order_release);
+  }
+
+private:
+  atomic_flag flag_;
+};
+
+// GLOBAL VARIABLES OF THE PROGRAM
+int total = 0;
+SpinLock sum_lock;
+
+/**
+ * @brief Function that sums array in distributed manner. 
+ * Each thread will do a local sum, then update the total value.
+ * This minimizes the time each thread stays in cr.
+ * 
+ * @param arr Complete array which will be sum up.
+ * @param start Start of the slice of array this thread will sum.
+ * @param end End of the slice of array this thread will sum.
+ */
+void sumArray(signed char* arr, int start, int end){
+    int localTotal = 0;
+    for (int i = start; i < end; i++) {
+      localTotal += arr[i];
+    }
+    // BEGIN OF CRITICAL REGION
+    sum_lock.acquire();
+    total += localTotal;
+    sum_lock.release();
+    // END OF CRITICAL REGION
+}
+
+/**
+ * @brief Creates array of random num, creates threads and splits array between them.
+ * Does all the testing for the different number of threads and size of arr
+ */
+int main() {
+
+    int arrExpoArraySize[3] = {7,8,9}; 
+    int arrNumThreads[9] = {1,2,4,8,16,32,64,128,256};
+
+    // Create and open a text file
+    ofstream MyFile("results.txt");
+
+    for (int a = 0; a < 3; a++){
+        int kExpoArraySize = arrExpoArraySize[a];
+        for (int t = 0; t < 9; t++){
+            int kNumThreads = arrNumThreads[t];
+            double total_time_taken = 0.0;
+            for (int m = 0; m < 10; m++){
+                cout << "array size: 10^" << kExpoArraySize;
+                cout << ", number of threads: " << kNumThreads <<endl;
+
+                clock_t start, end;
+                double time_taken;
+
+                // Creates array and fills it with random numbers
+                int kArraySize = pow(10, kExpoArraySize);
+                signed char* arr = new signed char[kArraySize];
+                srand(time(NULL));
+                for (int i = 0; i < kArraySize; i++) {
+                    int x = rand() % 201 - 100;
+                    signed char c = x;
+                    arr[i] = x;
+                }
+                // Gets expcted res to compare
+                int exp = 0;
+                for (int i = 0; i < kArraySize; i++) {
+                    exp+=arr[i];
+                }
+                cout<<"The expected result is: " << exp <<endl;
+
+                // Create threads
+                vector<thread> threads(kNumThreads);
+                start = clock();
+                int chunkSize = kArraySize / kNumThreads;
+                // Splits array and call function.
+                for (int i = 0; i < kNumThreads; i++) {
+                    int start = i * chunkSize;
+                    int end = (i == kNumThreads - 1) ? kArraySize : (i + 1) * chunkSize;
+                    threads[i] = thread(sumArray, arr, start, end);
+                }
+                // Waits for threads to finish.
+                for(auto& thread : threads){
+                    thread.join();
+                }
+                end = clock();
+
+                // Print results and exec time.
+                cout << "Total: " << total << endl;
+                if (total!=exp){
+                    cout<<"ERROR: Total obtained does not match the expected value!" <<endl;
+                }
+                time_taken = ((double) (end - start)) / CLOCKS_PER_SEC;
+                cout << "Time taken by program is : "  << time_taken  << " sec" << endl << endl;
+                total_time_taken += time_taken;
+                // Clean
+                delete[] arr;
+                total = 0.0;
+            }
+            // Write to the file
+            MyFile << total_time_taken/10 <<" ";
+            total_time_taken = 0.0;
+        }
+        MyFile << endl;
+    }
+    // Close the file
+    MyFile.close();
+    return 0;
+}
---
+++ TP2/Spinlock/plotter.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+
+nthreads = [1,2,4,8,16,32,64,128,256]
+results = []
+
+filename = "results.txt"
+
+with open(filename, "r") as file:
+    lines = file.readlines()
+
+for line in lines:
+    ints = [float(x) for x in line.split()]
+    results.append(ints)
+
+plt.figure(figsize=(12, 8))
+plt.plot(nthreads, results[0], 'o-', label='10^7')
+plt.plot(nthreads, results[1], 'o-', label='10^8')
+plt.plot(nthreads, results[2], 'o-', label='10^9')
+plt.xticks(nthreads)
+plt.yticks(np.arange(0,5.1, 0.5))
+plt.tick_params('x', labelsize = 6)
+for N in results:
+    plt.plot(nthreads[N.index(min(N))], min(N), 'o-', color = 'r')
+
+plt.xlabel('Number of Threads')
+plt.ylabel('Time (s)')
+plt.title('Runtime for sum of elements in an array', weight = 'bold')
+plt.legend()
+plt.savefig("results_spinlock.png")
+plt.show()
